6feb95273c9c38fc1e2e9c718c9b4ba25d5c71f1
kovbe11 <57264039+kovbe11@users.noreply.github.com>
2023-04-24 00:26:58 +0200

    Create modules with necessary dependencies

--- app/src/main/java/hu/bme/aut/android/bojackhorseman/di/RepositoryModule.kt
+++ app/src/main/java/hu/bme/aut/android/bojackhorseman/di/RepositoryModule.kt
@@ -1,10 +1,32 @@
 package hu.bme.aut.android.bojackhorseman.di
 
+import android.content.Context
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import hu.bme.aut.android.bojackhorseman.persistence.AppDatabase
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object RepositoryModule {
+
+
+        //        @Provides
+//        @Singleton
+//        fun provideCharactersDatabase(@ApplicationContext context: Context): AppDatabase {
+//            return Room.databaseBuilder(context, AppDatabase::class.java, "")
+//                .fallbackToDestructiveMigration()
+//                .build()
+//        }
+        @Provides
+        @Singleton
+        fun provideCharactersDatabase(@ApplicationContext context: Context) = AppDatabase()
+
+        @Provides
+        @Singleton
+        fun provideCharactersDAO(appDatabase: AppDatabase) = appDatabase.charactersDao()
+
 }
--- app/src/main/java/hu/bme/aut/android/bojackhorseman/persistence/AppDatabase.kt
+++ app/src/main/java/hu/bme/aut/android/bojackhorseman/persistence/AppDatabase.kt
@@ -1,4 +1,36 @@
 package hu.bme.aut.android.bojackhorseman.persistence
 
+import hu.bme.aut.android.bojackhorseman.model.Character
+
+//@Database(entities = [Character::class], exportSchema = false)
 class AppDatabase {
+    fun charactersDao(): CharactersDao = object : CharactersDao {
+        override suspend fun create(character: Character) {
+            //mock
+        }
+
+        override suspend fun findAll(): List<Character> {
+            val list = mutableListOf<Character>()
+            for (position in 1..20) {
+                list.add(
+                    Character(
+                        position,
+                        "Name $position",
+                        "Voice actor $position",
+                        "Some species"
+                    )
+                )
+            }
+            return list
+        }
+
+        override suspend fun update(character: Character) {
+            //mock
+        }
+
+        override suspend fun delete(character: Character) {
+            //mock
+        }
+
+    }
 }
